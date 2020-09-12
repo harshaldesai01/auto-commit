053fa9e619767f6a14b172080596240685b917dd
Harshal Desai <harshaldesai01@gmail.com>
2020-09-12 22:55:29 +0530

    first commit

---
+++ auto-commit.sh
@@ -0,0 +1,17 @@
+for file in *
+do 
+    sum1 = "$(md5dum "$file")"
+    sleep 60
+    sum2 = "$(md5sum "$file")"
+	
+    if [ "$sum1" = "$sum2" ];
+    then
+        echo "No changes!"
+    else
+	echo "Pushing changes..."
+        git add .
+        git commit -m "commiting changes at 'date + '%Y-%m-%d %H: %M: %S''"
+        git pull https://github.com/harshaldesai01/auto-commit
+        git push https://github.com/harshaldesai01/auto-commit
+        echo "Changes pushed!"
+    fi
